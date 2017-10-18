811b0568a53a35c64ffd2eb33f558a3fbcb10b19
lokin crook <carvingpixel@gmail.com>
2017-10-18 16:54:48 -0400

    Added Contact Class for Model

---
+++ ContactsWeb/Models/Contact.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactsWeb.Models
+{
+    public class Contact
+    {
+        // props
+
+        public int Id { get; set; }
+        public Guid UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhonePrimary { get; set; }
+        public string PhoneSecondary { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string StreetAdd { get; set; }
+        public string StreetAdd2 { get; set; }
+        public string City { get; set; }
+        public string state { get; set; }
+        public string zip { get; set; }
+
+    }
+}
